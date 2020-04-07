19b91859276edfb36fb6d8db6cf4fc8eacb91b71
Armin Kovacs <armin0191@gmail.com>
2020-04-07 14:33:40 +0200

    Fix video repo setup

--- videoservice/src/main/java/com/codecool/videoservice/repositories/VideoRepository.java
+++ videoservice/src/main/java/com/codecool/videoservice/repositories/VideoRepository.java
@@ -3,5 +3,5 @@ package com.codecool.videoservice.repositories;
 import com.codecool.videoservice.entities.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VideoRepository extends JpaRepository<Long, Video> {
+public interface VideoRepository extends JpaRepository<Video, Long> {
 }
