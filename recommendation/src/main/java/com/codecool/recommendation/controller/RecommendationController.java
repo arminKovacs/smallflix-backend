1386dc38862bd79091de3d02159ac5d21d1c9d53
Armin Kovacs <armin0191@gmail.com>
2020-04-09 17:07:24 +0200

    Fix data init in recommendation

--- recommendation/src/main/java/com/codecool/recommendation/controller/RecommendationController.java
+++ recommendation/src/main/java/com/codecool/recommendation/controller/RecommendationController.java
@@ -1,9 +1,12 @@
 package com.codecool.recommendation.controller;
 
+import com.codecool.recommendation.model.Video;
 import com.codecool.recommendation.service.VideosGetter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RecommendationController {
@@ -12,7 +15,7 @@ public class RecommendationController {
     private VideosGetter videosGetter;
 
     @GetMapping("/test")
-    public void test() {
-        videosGetter.getAllVideos();
+    public List<Video> test() {
+        return videosGetter.getAllVideos();
     }
 }
--- recommendation/src/main/java/com/codecool/recommendation/service/DataInitializer.java
+++ recommendation/src/main/java/com/codecool/recommendation/service/DataInitializer.java
@@ -1,8 +1,6 @@
 package com.codecool.recommendation.service;
 
 import com.codecool.recommendation.entity.Recommendation;
-import com.codecool.recommendation.model.Video;
-import com.codecool.recommendation.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,35 +15,28 @@ import java.util.List;
 public class DataInitializer {
 
     @Autowired
-    private VideosGetter videosGetter;
-
-    @Autowired
     private RecommendationService recommendationService;
 
     @Bean
     public CommandLineRunner createRecommendations() {
-        List<Video> videos = videosGetter.getAllVideos();
         return args -> {
             Recommendation drumRecommendation = Recommendation.builder()
                     .comment("This is a cool drumming video! Keep it up!")
                     .rating(5)
                     .build();
-            Long drumVideoId = videos.get(0).getId();
-            recommendationService.saveRecommendation(drumRecommendation, drumVideoId);
+            recommendationService.saveRecommendation(drumRecommendation, 1L);
 
             Recommendation funnyRecommendation = Recommendation.builder()
                     .comment("Ha ha very funny video! I love it!!")
                     .rating(5)
                     .build();
-            Long funnyVideoId = videos.get(1).getId();
-            recommendationService.saveRecommendation(funnyRecommendation, funnyVideoId);
+            recommendationService.saveRecommendation(funnyRecommendation, 2L);
 
             Recommendation gamingRecommendation = Recommendation.builder()
                     .comment("Nice gameplay bro!")
                     .rating(5)
                     .build();
-            Long gamingId = videos.get(2).getId();
-            recommendationService.saveRecommendation(gamingRecommendation, gamingId);
+            recommendationService.saveRecommendation(gamingRecommendation, 3L);
         };
     }
 }
