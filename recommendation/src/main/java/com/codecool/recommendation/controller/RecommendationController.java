87c9aa1d36d229955eebb8fd853065030893eaac
Armin Kovacs <armin0191@gmail.com>
2020-04-10 09:32:05 +0200

    Create method to get all recommendations for video id

--- recommendation/src/main/java/com/codecool/recommendation/controller/RecommendationController.java
+++ recommendation/src/main/java/com/codecool/recommendation/controller/RecommendationController.java
@@ -1,9 +1,12 @@
 package com.codecool.recommendation.controller;
 
+import com.codecool.recommendation.entity.Recommendation;
 import com.codecool.recommendation.model.Video;
+import com.codecool.recommendation.repository.RecommendationRepository;
 import com.codecool.recommendation.service.VideosGetter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +17,11 @@ public class RecommendationController {
     @Autowired
     private VideosGetter videosGetter;
 
-    @GetMapping("/test")
-    public List<Video> test() {
-        return videosGetter.getAllVideos();
+    @Autowired
+    private RecommendationRepository recommendationRepository;
+
+    @GetMapping("/{id}/list-all-recommendation")
+    public List<Recommendation> getAllRecommendationsByVideoId(@PathVariable Long id) {
+        return recommendationRepository.getAllByVideoId(id);
     }
 }
